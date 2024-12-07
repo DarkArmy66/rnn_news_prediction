b5d369e6988eee50c7a5ce90b9ede09359aab821
DarkArmy66 <wungerer@uni-osnabrueck.de>
2024-12-07 20:50:46 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,13 +1,1 @@
-@echo off
-:: Create a virtual environment
-python -m venv venv
-
-:: Activate the virtual environment
-call venv\Scripts\activate
-
-:: Install the required packages
-pip install -r requirements.txt
-
-:: Start Jupyter Notebook
-jupyter notebook
 
